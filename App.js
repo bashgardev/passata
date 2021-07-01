d0d1ed867dfcc0a98c3d723304bf4d403210f7ec
Sebastian Garcia <bashgar.dev@gmail.com>
2021-07-01 13:00:24 -0400

    Fixed timer logic to be more accurate by calculating difference in time. Also avoids having to run background tasks as timer updates automatically with passing time

--- App.js
+++ App.js
@@ -7,7 +7,10 @@ import { Appearance } from "react-native";
 
 export default function App() {
   const [timerOn, setTimerOn] = useState(false);
-  const [timer, setTimer] = useState(0);
+  const [timer, setTimer] = useState(1500000);
+
+  // const [startTime, setStartTime] = useState(0);
+  // const [endTime, setEndTime] = useState(0);
 
   // determine if environment is dark mode or not
   let isDarkMode = false;
@@ -18,23 +21,40 @@ export default function App() {
 
   useEffect(() => {
     let interval = null;
-
     if (timerOn) {
+      // setStartTime(Date.now());
+      let st = Date.now();
+      let et;
+
       interval = setInterval(() => {
-        // time increment 1s in miliseconds
-        setTimer((prevTime) => prevTime + 1000);
-      }, 1000);
+        // setEndTime(Date.now());
+        et = Date.now();
+        setTimer(timer - (et - st));
+      }, 200);
     } else {
-      clearInterval(interval);
+      stopTimer();
     }
-    return () => clearInterval(interval);
-  }, [timerOn, timer]);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timerOn]);
 
   // handler function
+  const startTimer = () => {
+    setTimerOn(true);
+  };
+
+  const stopTimer = () => {
+    setTimerOn(false);
+  };
+
   const resetTimer = () => {
     setTimerOn(false);
-    setTimer(0);
+    setTimer(1500000);
+    // setStartTime(0);
+    // setEndTime(0);
   };
+
   return (
     // try using css from styled-components
     <View style={isDarkMode ? styles.containerDark : styles.containerLight}>
@@ -46,25 +66,28 @@ export default function App() {
               : { fontSize: 80, color: "grey" }
           }
         >
-          {/* {timer + "   "} */}
           {("0" + Math.floor((timer / 60000) % 60)).slice(-2)}:
           {("0" + Math.floor((timer / 1000) % 60)).slice(-2)}
         </Text>
       </View>
+      {/* <Text>timerOn: {timerOn ? "true" : "false"}</Text>
+      <Text>startTime: {startTime}</Text>
+      <Text>endTime: {endTime}</Text>
+      <Text>delta: {timer}</Text> */}
 
       <View style={styles.buttonSection}>
         {timerOn ? (
           <AppButton
             width="60%"
             style={styles.stopButton}
-            onPress={() => setTimerOn(false)}
+            onPress={stopTimer}
             title="Stop"
           ></AppButton>
         ) : (
           <AppButton
             width="60%"
             style={styles.startButton}
-            onPress={() => setTimerOn(true)}
+            onPress={startTimer}
             title="Start"
           ></AppButton>
         )}
